431cde37a8ec2cca91ffd2c631cc2ecc63e6b026
mauroslucios <mauroslucios@gmail.com>
2023-07-30 18:19:54 -0300

    criando EC2 para acesso http/ssh

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "associate_vpc_id" {
+  default = "vpc-0a5040e2a944b2d33"
+}
+
+variable "associate_subnet_public_id" {
+  default = "subnet-0d018a6069cf625ef"
+}
+
+variable "associate_security_group" {
+  default = "sg-0358fa4606f1faccd"
+}
+
+variable "ami_key_pair_name" {
+  default = "luciosmauros_treinos"
+}
